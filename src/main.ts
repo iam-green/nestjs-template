7a0edb25e72dbff3fd8baf3c83ce58f026e5a2e4
iam-green <yundohyun050121@gmail.com>
2024-08-06 18:20:56 +0900

    edit: `main.ts` global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      ...(process.env.NODE_ENV == 'production' && {
+        disableErrorMessages: true,
+      }),
+    }),
+  );
   await app.listen(+process.env.SERVER_PORT || 3000);
 }
 bootstrap();
